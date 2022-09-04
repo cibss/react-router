58ab1224e747080a1942a238285f4117b6244d10
cibss <cibss13@gmail.com>
2022-09-04 17:20:25 +0700

    refactor nested routes

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,35 +1,43 @@
 import { Fragment } from "react";
-import { useParams, Route } from "react-router-dom";
+import { useParams, Route, Link, useRouteMatch } from "react-router-dom";
 
-import Comments from '../components/comments/Comments'
+import Comments from "../components/comments/Comments";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 
 const DUMMY_QUOTES = [
   {
-    id: 'q1',
-    author: 'Max',
-    text: 'Learning React is fun!'
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
   },
   {
-    id: 'q2',
-    author: 'Kevin',
-    text: 'Learning React is great!'
-  }
-]
+    id: "q2",
+    author: "Kevin",
+    text: "Learning React is great!",
+  },
+];
 
 const QuoteDetail = () => {
+  const match = useRouteMatch();
   const params = useParams();
-  
-  const quote = DUMMY_QUOTES.find(quote => quote.id === params.quoteId);
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
   if (!quote) {
-    return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
 
   return (
     <Fragment>
       <h1>Quote detail</h1>
       <HighlightedQuote text={quote.text} author={quote.author} />
-      <Route path='/quotes/:quoteId/comments'>
+      <Route path={match.path} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`${match.url}/comments`}>
+            Load comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`${match.path}/comments`}>
         <Comments />
       </Route>
     </Fragment>
